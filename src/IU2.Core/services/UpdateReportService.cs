d5a352ef0062e7b460f8ff2d7efc650527d245a0
Emelie Klint <emeliee.klint@gmail.com>
2026-09-25 13:35:46 +0200

    added seed data in UpdateReportService

---
+++ src/IU2.Core/services/UpdateReportService.cs
@@ -0,0 +1,18 @@
+using IU2.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IU2.Core.services
+{
+    internal class UpdateReportService
+    {
+        private List<TimeReport> _reports = new()
+    {
+        new TimeReport { Id = 1, Kund = "Acme AB", Datum = new DateOnly(2026, 9, 8), Timmar = 4.0m, Beskrivning = "Utveckling av ny funktion" },
+        new TimeReport { Id = 2, Kund = "Globex Solutions", Datum = new DateOnly(2026, 9, 8), Timmar = 2.5m, Beskrivning = "Bugfixar och testning" },
+        new TimeReport { Id = 3, Kund = "Initech", Datum = new DateOnly(2026, 9, 8), Timmar = 1.0m, Beskrivning = "Möte med kund" }
+    };
+
+    }
+}
