1e8c4ef9f34d9d74b0999dd7d4c653c148b7cf4f
Cazperrr <cazper.e@hotmail.com>
2026-09-25 14:35:13 +0200

    added Id and Description to TimeReport-model

--- src/IU2.Core/Models/TimeReport.cs
+++ src/IU2.Core/Models/TimeReport.cs
@@ -6,8 +6,10 @@ namespace IU2.Core.Models
 {
     public class TimeReport
     {
+        public int Id { get; set; }
         public int CustomerId { get; set; }
         public DateTime Date { get; set; }
         public decimal Hours { get; set; }
+        public string? Description { get; set; }
     }
 }
